7679ce8e21434a4f4a42c4ab112cc22fb62a097b
muhammedashraf9244 <hom9244.9244@gmail.com>
2021-02-16 08:41:06 +0200

    Finish Lecture 3

--- __manifest__.py
+++ __manifest__.py
@@ -4,12 +4,14 @@
     'description':'''Hms is a module for record patients''',
     'author':"MuhammedAshraf",
     'company':"ITI",
+    'website':"https://github.com/muhammedashraf9244/Hostiptal-Management-Odoo",
     'version':"13.0",
     'data':[
       'views/patient_view.xml',
       'views/doctors_view.xml',
       'views/department_view.xml',
+      'views/crm_inherit_view.xml',
     ],
-    'depends': ['base'],
+    'depends': ['base','crm'],
 
 }
--- models/patient.py
+++ models/patient.py
@@ -1,10 +1,15 @@
 from odoo import models,fields,api
+from odoo.exceptions import UserError
+import re
+from datetime import  datetime
 class HmsPatient(models.Model):
     _name = "hms.patient"
     _rec_name = 'first_name'
     first_name=fields.Char(string="First Name",required=True)
     last_name=fields.Char(string="Last Name",required=True)
+    email = fields.Char()
     birth_date=fields.Date()
+    age= fields.Integer(compute="get_age",store=True)
     history=fields.Html()
     cr_ratio=fields.Float(string="CR Ratio",required=True)
     blood_type=fields.Selection([("A","Group A"),("B","Group B")
@@ -17,18 +22,74 @@ class HmsPatient(models.Model):
     doctor_id=fields.Many2many('hms.doctors')
     dep_capcity=fields.Integer(related='department_id.capcity',string="department capcity")
     history_id=fields.One2many("history.patient.line","patient_id")
-    status=["Undetermined","Good","Fair", "Serious"]
+    state = fields.Selection([
+        ('undetermined','Undetermined'),('good','Good'),('fair','Fair'),('serious','Serious')
+    ],default='undetermined')
+
+
+    _sql_constraints = [
+        ("Invalid email","unique(email)","This email is already exits")
+    ]
+
+    @api.constrains("email")
+    def check_email(self):
+        my_pattern = r'^[a-zA-Z0-9\.]+@[a-z0-9]+\.(com|org|net)$'
+        is_match = re.match(my_pattern,self.email)
+        if not is_match:
+            raise UserError(f'This email {self.email} is Invalid')
+
+    # Action of buttons of state
+    def set_good(self):
+        for record in self:
+            record.state = 'good'
+
+    def set_fair(self):
+        for record in self:
+            record.state = 'fair'
+    def set_serious(self):
+        for record in self:
+            record.state = 'serious'
+
+    def back_to_undetermined(self):
+        for record in self:
+            record.state = 'undetermined'
+
+
+    # compute age
+    @api.onchange("birth_date")
+    def get_age(self):
+        for record in self:
+            if record.birth_date:
+                # print(str(record.birth_date))
+                # convert date type to datetime
+                birth_date_time = datetime.strptime(str(record.birth_date), "%Y-%m-%d")
+                # print(birth_date_time)
+                # calc differance between two dates
+                record.age =abs((birth_date_time - datetime.now()).days) // 365
+
+
+    @api.constrains('state')
+    def write_new_log(self):
+        for record in self:
+            record.env['history.patient.line'].create({'patient_id':record.id,
+                                                 'description':f'Add new state {record.state}'})
 
     @api.onchange('age')
     def _onchange_age(self):
-        if self.age < 30:
-            self.p_c_r=True
-            return {
-                'warning': {
-                    'title': "PCR Checked",
-                    'message': "PCR automatically checked",
+        for record in self:
+            if record.age==0:
+                return
+            elif record.age < 30:
+                record.p_c_r=True
+                return {
+                    'warning': {
+                        'title': "PCR Checked",
+                        'message': "PCR automatically checked",
+                    }
                 }
-            }
+            elif record.age >= 30:
+                record.p_c_r=False
+                return
 
 
 
@@ -40,3 +101,20 @@ class HistoryPatientLine(models.Model):
 
 
 
+class CrmInheritPartner(models.Model):
+    _inherit = 'res.partner'
+    related_user_id = fields.Many2one('hms.patient')
+
+    @api.constrains("email")
+    def check_email(self):
+        for record in self:
+            email_patient = record.env['hms.patient'].search([('email','=',record.email)])
+            if email_patient:
+                raise UserError(f'This email {record.email} is exits in patients ')
+
+    # @api.multi is deprcated in odoo 13 so when use nlink use with for each element
+    def unlink(self):
+        for record in self:
+            if record.related_user_id:
+                raise UserError(f'Be ware this customer related with patient name {record.related_user_id.first_name}')
+        return super(CrmInheritPartner, self).unlink()
